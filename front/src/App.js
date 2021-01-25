e62581ae96d33138ccc2a98269afa6638515d3b4
vahideHNF <48863169+vahideHNF@users.noreply.github.com>
2021-01-25 17:31:26 +0330

    sign up page added

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Signup from "./Signup";
+import Login from "./Login";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/sign-up">
+          <Button type="primary">Sign up</Button>
+        </Link>
+        <Link to="/log-in">
+          <Button type="default">Log in</Button>
+        </Link>
+        <Switch>
+          <Route path="/log-in" component={Login} />
+          <Route path="/sign-up" component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
